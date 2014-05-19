98e80a315e8879efb4169c6e8b02e3bd0e94e6bc
Sarvex Jatasra <sarvex.jatasra@gmail.com>
2014-05-19 13:06:35 +0530

    Multimedia Playbak

---
+++ Action/MediaPlayer/gen/com/simplyjat/mediaplayer/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.simplyjat.mediaplayer;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Action/README.md
@@ -0,0 +1,4 @@
+Android in Action
+---------
+
+**Multimedia** – Enter a string and the program will reverse it and print it out.
